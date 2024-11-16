dc6b55193fd7a22c39276a86bbee423ca1a0bebc
Josh <josh.si@outlook.com>
2024-11-16 10:03:03 -0500

    adding instead of subtracting index

--- components/journal/WeekBar.jsx
+++ components/journal/WeekBar.jsx
@@ -18,7 +18,7 @@ export default function WeekBar({ entries, startDate, currentDate }) {
   // Create dates for the current week
   const weekDates = daysOfWeek.map((_, index) => {
     const date = new Date(weekStartDate);
-    date.setDate(weekStartDate.getDate() + (index - weekStartDate.getDay()));
+    date.setDate(weekStartDate.getDate() + (index + weekStartDate.getDay()));
     return date;
   });
 
